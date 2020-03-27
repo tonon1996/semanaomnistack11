5a7aed7b47cd4afc56defd0c7df4f97e9eef02ff
tonon1996 <tonon1996@gmail.com>
2020-03-27 12:09:07 -0300

    font-end da aplicação

--- backend/src/controllers/SessionControllers.js
+++ backend/src/controllers/SessionControllers.js
@@ -11,7 +11,7 @@ module.exports = {
         if (!ong) {
             return response.status(400).json({error: "No ONG found with this ID"});
         }
-
+        
         return response.json(ong);
     }
 }
